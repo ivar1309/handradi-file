0427c5f9c6c3e6721d19fda3a1aa117aab883a26
Ívar Árnason <ivar1309@gmail.com>
2025-08-15 12:15:09 +0000

    fix

--- service.ts
+++ service.ts
@@ -86,7 +86,7 @@ class HandradiFileProviderService extends AbstractFileProviderService {
         file: FileTypes.ProviderGetPresignedUploadUrlDTO
     ): Promise<FileTypes.ProviderFileResultDTO> {
         this.logger_.info(`Handradi: presigned upload url -> ${file.filename}`)
-        let res = this.client.getPresignedUploadUrl(file)
+        let res = await this.client.getPresignedUploadUrl(file)
         this.logger_.info(`Handradi: getPresignedUploadUrl -> ${res}`)
         return res
     } 
